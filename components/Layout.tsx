ba80683099a425099acbca39d8e31e348c6540cd
Matshiro <daniel.b.lewinski@gmail.com>
2023-07-18 11:22:15 +0200

    first commit :D

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+function Layout({ children }): React.FC<LayoutProps> {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
