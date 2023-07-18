f9cd3c70db95fb489b480cc6dbfcc60ba82eeef9
Matshiro <daniel.b.lewinski@gmail.com>
2023-07-18 17:42:28 +0200

    Added header and FollowBar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Header from "@/components/Header"
 
 function index() {
   return (
-    <div className="text-3xl text-sky-500">Hello world</div>
+    <>
+      <Header label="Home"/>
+    </>
   )
 }
 
