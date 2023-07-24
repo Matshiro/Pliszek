3dc330c5cfe87dfd4d01ec439abae092b795e886
Matshiro <daniel.b.lewinski@gmail.com>
2023-07-25 00:52:46 +0200

    Added basic tweet functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import Header from "@/components/Header"
+import Form from "@/components/Form";
+import Header from "@/components/Header";
 
 function index() {
   return (
     <>
-      <Header label="Główna"/>
+      <Header label="Główna" />
+      <Form placeholder="Co ptaki śpiewają?" />
     </>
-  )
+  );
 }
 
-export default index
+export default index;
