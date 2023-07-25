2322f7a48fe645e0de939e434518049d517fa77a
Matshiro <daniel.b.lewinski@gmail.com>
2023-07-25 20:13:44 +0200

    Added posting and viewing posts on main site and on account profile

---
+++ components/posts/PostFeed.tsx
@@ -0,0 +1,18 @@
+import usePosts from "@/hooks/usePosts";
+import PostItem from "./PostItem";
+
+interface PostFeedProps {
+  userId?: string;
+}
+function PostFeed({ userId }: PostFeedProps) {
+  const { data: posts = [] } = usePosts(userId);
+  return (
+    <>
+      {posts.map((post: Record<string, any>) => (
+        <PostItem userId={userId} key={post.id} data={post} />
+      ))}
+    </>
+  );
+}
+
+export default PostFeed;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Form from "@/components/Form";
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 
 function index() {
   return (
     <>
       <Header label="Główna" />
       <Form placeholder="Co ptaki śpiewają?" />
+      <PostFeed />
     </>
   );
 }
